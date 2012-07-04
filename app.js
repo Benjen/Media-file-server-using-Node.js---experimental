4ae585e2e8e3dc528264ab758aea28b3d5cc0794
Benjen <benjen@benjen.com>
2012-07-04 12:07:12 +0800

    Revert "Added database models for user and movie."
    This reverts commit cab8539fdf029af0b355aba9d67d217441fac32a.
    
    Conflicts:
    
    	models.js

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var express = require('express')
   , files = {} // Holds info on files currently being uploaded.
   , fs = require('fs')
   , exec = require('child_process').exec
-  , models = require('./models')
-  , mongoose = require('mongoose')
   , util = require('util');
 
 var app = module.exports = express.createServer();
@@ -30,21 +28,10 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.set('db-uri', 'mongodb://localhost/media-server');
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
-});
-
-/*
- * Database models
- */ 
-
-models.defineModels(mongoose, function() {
-  app.User = User = mongoose.model('User');
-  app.Movie = Movie = mongoose.model('Movie');
-  db = mongoose.connect(app.set('db-uri'));
 });
 
 /*
@@ -54,11 +41,11 @@ models.defineModels(mongoose, function() {
 // Initialize socket.io.
 io = require('socket.io').listen(app);
 
-// Set io connection events.
+// Set on connection event.
 io.sockets.on('connection', function (socket) {
   
-  // Start uploading process. @param data contains the variables 
-  // passed through from the html file.
+  // Start uploading process
+  //   @param data contains the variables passed through from the html file.
   socket.on('start', function (data) { 
     var name = data['name'];
     var size = data['size'];
@@ -94,7 +81,7 @@ io.sockets.on('connection', function (socket) {
     };
     var place = 0;
     try {
-      // Check if file is already present on server.
+      // Is the file already present on server.
       var stat = fs.statSync('temp/' +  name);
       if (stat.isFile()) {
         // Update file information with size of already uploaded portion of file.
@@ -122,7 +109,6 @@ io.sockets.on('connection', function (socket) {
     });
   });
   
-  // Upload
   socket.on('upload', function (data) {
     var name = data['name'];
     files[name]['downloaded'] += data['data'].length;
